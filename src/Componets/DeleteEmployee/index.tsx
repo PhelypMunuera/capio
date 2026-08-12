8c438df49d5e602286c8d4b183bb993a7e81137a
Phelyp Munuera <mphelyp@gmail.com>
2026-08-12 16:41:48 -0300

    feat: general structure

--- src/Componets/DeleteEmployee/index.tsx
+++ src/Componets/DeleteEmployee/index.tsx
@@ -1,8 +1,21 @@
+import style from './style.module.css'
+import { demoDialog } from '../Dialog/Handler'
 export function DeleteEmployee() {
-    return(
-        <>
-      asdasd
-        </>
-    )
+  return (
+    <div className={style.container}>
+      <h1>Confirme esta ação?</h1>
+      <div className={style.InformationsArea}>
+        <p>Confirma a exclusão dos dados de acesso
+        do funcionário <strong>Phelyp Munuera da Silva</strong>,
+        matrícula <strong>4905-7</strong>?</p>
+
+        <span>Esta ação removerá o acesso do funcionário ao sistema.</span>
+      </div>
+      <div className={style.containerButton}>
+        <button>Confirmar</button>
+        <button onClick={() => { demoDialog.close() }}>Cancelar</button>
+      </div>
+    </div>
+  )
 
 }
--- src/Componets/EditEmployee/index.tsx
+++ src/Componets/EditEmployee/index.tsx
@@ -1,8 +1,25 @@
+import style from './style.module.css'
+import { demoDialog } from '../Dialog/Handler'
+import { InputPassword } from '../InputPassword'
+import { Input } from '../Input'
 export function EditEmployee() {
-    return(
-        <>
-        32323
-        </>
-    )
+  return (
+    <div className={style.container}>
+      <h1>Confirme esta ação?</h1>
+      <div className={style.InformationsArea}>
+        <div className={style.name}>Lucas Henrique da Silva</div>
+        
+        <div className={style.dataCredentials}>
+            <div className={style.registration}>32154</div>
+            <Input value={'qwer@2345'} /> 
+        </div>    
+        <InputPassword value={12345678}/>
+      </div>
+      <div className={style.containerButton}>
+        <button>Salvar</button>
+        <button onClick={() => { demoDialog.close() }}>Cancelar</button>
+      </div>
+    </div>
+  )
 
 }
